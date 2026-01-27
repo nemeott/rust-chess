27a0c52e8e7908df799b647c8d017686ce3e1a46
nemeott <123220311+nemeott@users.noreply.github.com>
2026-01-27 16:44:22 -0500

    Rust nightly shell for future reference

--- shell.nix
+++ shell.nix
@@ -2,13 +2,24 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+let
+  # Rust nightly (for parallel building)
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {
+    inherit pkgs;
+  };
+
+  rustToolchain = fenix.complete.withComponents [
+    "cargo"
+    "clippy"
+    "rust-src"
+    "rustc"
+    "rustfmt"
+    "rust-analyzer"
+  ];
+in
 pkgs.mkShell {
   packages = with pkgs; [
-    rustc
-    rust-analyzer
-    cargo
-    rustfmt
-    clippy
+    rustToolchain
 
     python313
     python313Packages.pip
